7ee13af2738f42ce0771725bb1d6a19aa7c16db6
sanzx777 <victorsan1550@gmail.com>
2025-08-28 20:55:15 -0300

    aula python03

---
+++ aula3/ex02.py
@@ -0,0 +1,7 @@
+list [10, 20, 30, 40, 50]
+
+list.insert(1,15) 
+print(list)
+
+list.append(60)
+print(list)
---
+++ aula3/ex03.py
@@ -0,0 +1,4 @@
+listAlunos = ['Victor', 'Pedro', 'Lucas', 'Carlos', 'Jorge']
+for listAlunos in listAlunos:
+    print(f"nome do aluno: {listAlunos}")
+
